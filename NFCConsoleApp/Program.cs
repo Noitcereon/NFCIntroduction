34113524e83b7e972ce49799d90f41df2d805ade
Thomas (Noit) <tba@live.dk>
2021-10-19 13:52:44 +0200

    Add project files.

---
+++ NFCConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NFCConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            Console.WriteLine("Worker starting");
+            worker.Start();
+
+            Console.WriteLine("Worker finished\n" +
+                              "Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NFCConsoleApp/Worker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using NdefLibrary.Ndef;
+
+namespace NFCConsoleApp
+{
+    public class Worker
+    {
+        public void Start()
+        {
+            try
+            {
+                // Simply using the string type works as well
+                NdefTextRecord ndefTextRecord = new NdefTextRecord { Text = Console.ReadLine() };
+
+                if (ndefTextRecord.Text == "0716656832")
+                {
+                    Console.WriteLine("The white chip was scanned");
+                    return;
+                }
+
+                Console.WriteLine("Something other than the white chip was entered.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
